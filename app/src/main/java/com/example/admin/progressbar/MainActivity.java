c2a40f2710d4a4eba4916ba2d2678559af5f6c81
joseph alemu <joseph.alemu01@gmail.com>
2018-01-26 21:34:57 -0500

    working  perfect commit two

--- app/src/main/java/com/example/admin/progressbar/MainActivity.java
+++ app/src/main/java/com/example/admin/progressbar/MainActivity.java
@@ -4,31 +4,48 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
-    private static int progress;
+
     private ProgressBar progressbarOne;
     private ProgressBar progressbarTwo;
     private ProgressBar progressbarThree;
     private ProgressBar progressbarFour;
     private int progressStatus = 0;
-    private Handler handler = new Handler();
+    private Handler handler ;
+
+
 
     ExecutorService executorService = null;
+    private TextView tvFour;
+    private TextView tvThree;
+    private TextView tvTwo;
+    private TextView tvOne;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        progress = 0;
+
+
+        handler = new Handler(getApplicationContext().getMainLooper());
+
         progressbarOne = (ProgressBar) findViewById(R.id.progressbarOne);
         progressbarTwo = (ProgressBar) findViewById(R.id.progressbarTwo);
         progressbarThree = (ProgressBar) findViewById(R.id.progressbarThree);
         progressbarFour = (ProgressBar) findViewById(R.id.progressbarFour);
+        tvOne = (TextView)findViewById(R.id.tvOne);
+        tvTwo = (TextView)findViewById(R.id.tvTwo);
+        tvThree = (TextView)findViewById(R.id.tvThree);
+        tvFour = (TextView)findViewById(R.id.tvFour);
         /*The first Progress Bar*/
         progressbarOne.setVisibility(View.VISIBLE);
         progressbarOne.setProgress(0);
@@ -48,17 +65,120 @@ public class MainActivity extends AppCompatActivity {
         progressbarFour.setVisibility(View.VISIBLE);
         progressbarFour.setProgress(0);
         progressbarFour.setMax(100);
-        MyTask myTask1 = new MyTask( 500);
-        MyTask myTask2 = new MyTask( 300);
-        MyTask myTask3 = new MyTask( 100);
-        MyTask myTask4 = new MyTask( 200);
-        executorService = Executors.newFixedThreadPool(4);
-        executorService.execute(myTask1);
-        executorService.execute(myTask2);
-        executorService.execute(myTask3);
-        executorService.execute(myTask4);
 
 
-        executorService.shutdown();
+
+        ExecutorService execService = Executors.newFixedThreadPool(4);
+
+        /*  progress bar  One  */
+
+        execService.execute(new Runnable() {
+            int  count =0;
+            @Override
+            public void run() {
+
+                while(count  <100){
+                    try {
+                        ++count;
+                        progressbarOne.setProgress(count);
+                        tvOne.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                tvOne.setText(count+"%");
+                            }
+                        });
+
+                        Thread.sleep(150);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+         /*  progress bar  Two  */
+        execService.execute(new Runnable() {
+            int  count =0;
+            @Override
+            public void run() {
+
+                while(count  <100){
+                    try {
+                        ++count ;
+                        progressbarTwo.setProgress(count);
+
+
+                        tvTwo.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                tvTwo.setText(count+"%");
+                            }
+                        });
+                        Thread.sleep(200);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+          /*  progress bar  Three  */
+        execService.execute(new Runnable() {
+
+            int  count =0;
+            @Override
+            public void run() {
+
+                while(count  <100){
+                try {
+                    ++count ;
+                    progressbarThree.setProgress(count);
+
+
+                    tvThree.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            tvThree.setText(count+"%");
+                        }
+                    });
+                    Thread.sleep(180);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                }
+            }
+        });
+
+         /*  progress bar  Four  */
+        execService.execute(new Runnable() {
+            int  count =0;
+            @Override
+            public void run() {
+
+                while(count  <100){
+                    try {
+                        ++count;
+                        progressbarFour.setProgress(count);
+                        tvThree.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                tvFour.setText(count+"%");
+                            }
+                        });
+                        Thread.sleep(110);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+
+
+
     }
+
+
+
 }
+
